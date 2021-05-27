48463259ce927e731745960255b81a96eb355236
adrianvolkmann <69943161+adrianvolkmann@users.noreply.github.com>
2021-05-26 23:16:11 -0300

    limpado lixo

--- src/main/java/estudandoGit/Principal.java
+++ src/main/java/estudandoGit/Principal.java
@@ -3,17 +3,8 @@ package estudandoGit;
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		
-		
-		System.out.println("Ola Git!");
-		
-		System.out.println("Mergeado para Master!");
-		
-		System.out.println("Nova Linha!");
-		
-		System.out.println("BranchC");
+
+		System.out.println("Apagado o lixo!");
 		
 	}
-	
 }
